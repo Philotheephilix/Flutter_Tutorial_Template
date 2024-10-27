f3be8c03489c38226aacf8c5203406ef9cc19c86
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-10-27 19:18:34 +0530

    screenshot protection

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:no_screenshot/no_screenshot.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false, //Removed debug Banner on the top right
-    home: Homepage(),
-    ));
+  runApp(const MyApp());
 }
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _noScreenshot = NoScreenshot.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _noScreenshot.screenshotStream.listen((value) {
+      setState(() {
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Digital Privacy Protection Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () async {
+                  bool result = await _noScreenshot.screenshotOff();
+                  debugPrint('Screenshot Off: $result');
+                },
+                child: const Text('Disable Screenshot'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  bool result = await _noScreenshot.screenshotOn();
+                  debugPrint('Enable Screenshot: $result');
+                },
+                child: const Text('Enable Screenshot'),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
